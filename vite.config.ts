43a1f65f9bb667f34de7c00a67744d979a4a3784
Kevin Cox <5299040+PointOfNullReturn@users.noreply.github.com>
2025-11-26 08:48:38 -0600

    fix: Updated base in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './neoprism',
+  base: '/neoprism/',
   plugins: [react()],
   resolve: {
     alias: {
